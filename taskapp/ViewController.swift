cd4a9167c74b45c11b72715193e4d6c92d7757c9
sari.hatanaka <s-hatanaka@i-enter.co.jp>
2020-04-22 13:47:49 +0900

    TaskクラスにcategoryというStringプロパティを追加しました。 タスク作成画面でcategoryを入力できるようにしました。 一覧画面に文字列検索用の入力欄を設置し、categoryと合致するTaskのみ絞込み表示するようにしました。

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,27 +7,83 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
     
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+
+    
+    let realm = try! Realm()
+
+  
+
+    
+//　　 DB内のタスクが格納されるリスト
+//    日付の近い順でソート：昇順
+//    以降内容をアップデートするとリスト内は自動的に更新される
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date",
+    ascending: true)
+    
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        
         tableView.delegate = self
-        tableView.dateSource = self
+        tableView.dataSource = self
     
     
+        searchBar.delegate = self
+       
+        
     }
+    
+    
+    /// segueで画面遷移する時に呼ばれる
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let inputViewController:inputViewController = segue.destination as! inputViewController
+     
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        } else {
+            let task = Task()
+            
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0 {
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+           
+            inputViewController.task = task
+        }
+    }
+  
+    
+    /// 入力から戻ってきた時にTableViewを更新させる
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    
 
+   
+    
+    
+    
+    
     
     /// セルの数を返すメソッド
-    func tableView(_tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return taskArray.count
+          
     }
     
     
@@ -36,14 +92,22 @@ class ViewController: UIViewController {
         // 再利用可能な cell を得る
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text =  task.title
+        
+        let  formatter = DateFormatter()
+        formatter.dateFormat = "yyy-MM-dd HH:mm"
+        
+        let  dateString:String = formatter.string(from: task.date)
+        cell.detailTextLabel?.text = dateString
+        
         return cell
     }
 
     
     /// 各セルを選択した時に実行されるメソッド
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        performSegue(withIdentifier: "cellSegue", sender: nil)
+        performSegue(withIdentifier: "cellSegue",sender: nil)
         
     }
     
@@ -58,6 +122,77 @@ class ViewController: UIViewController {
     /// Delete ボタンを押された時に呼ばれるメソッド
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
+        if editingStyle == .delete {
+         
+//            削除するタスクを取得する
+            let task = self.taskArray[indexPath.row]
+            
+//            ローカル通知をキャンセルする
+            let center = UNUserNotificationCenter.current()
+            center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
+            
+//            データベースから削除する
+            try! realm.write{
+                self.realm.delete(self.taskArray[indexPath.row])
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            
+//            未通知のローカル通知一覧をログ出力
+            center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+            for request in requests {
+                print("/---------------")
+                print(request)
+                print("---------------/")
+            }
+        }
+        
+        
     }
     
 }
+    
+    /// 検索ボタンがタップされた時
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        print("hoge")
+      
+        
+        guard let inputCategoryField = searchBar.text, !inputCategoryField.isEmpty else {
+
+           self.taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)
+
+           tableView.reloadData()
+
+           return
+
+       }
+
+       let predicate = NSPredicate(format: "category = %@", inputCategoryField)
+
+       self.taskArray = realm.objects(Task.self).filter(predicate).sorted(byKeyPath: "category", ascending: true)
+
+       //print(taskArray)
+
+    
+       tableView.reloadData()
+
+        
+        self.view.endEditing(true)
+    
+        
+               
+        
+      
+    }
+    
+   
+    
+   
+}
+
+
+
+
+
+
+
--- taskapp/inputViewController.swift
+++ taskapp/inputViewController.swift
@@ -7,19 +7,105 @@
 //
 
 import UIKit
+import RealmSwift
+import UserNotifications
 
 class inputViewController: UIViewController {
 
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var categoryTextField: UITextField!
+    
+    
+    
+    let realm = try! Realm()
+    var task: Task!
+    var category: Task!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する（キーボードを閉じる）
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector (dismissKeyboard))
+        //  viewにインスタンスを追加
+        self.view.addGestureRecognizer(tapGesture)
+        
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        datePicker.date = task.date
+        categoryTextField.text = task.category
         // Do any additional setup after loading the view.
     }
     
+    
+    
+    /// 更新して保存
+      override func viewWillDisappear(_ animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titleTextField.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.datePicker.date
+            self.task.category = self.categoryTextField.text!
+            self.realm.add(self.task, update: .modified)
+        }
+        setNotification(task: task)
+        
+        super.viewWillDisappear(animated)
+    }
+    
+    
+   
+        
+    
+    
+    
+//    タスクのローカル通知を登録する
+    func setNotification(task: Task) {
+        let content = UNMutableNotificationContent()
+        
+        if task.title == "" {
+            content.title = "(タイトルなし)"
+        } else {
+            content.title = task.title
+        }
+        if task.contents == "" {
+            content.body = "(内容なし)"
+        }else {
+            content.body = task.contents
+        }
+        content.sound = UNNotificationSound.default
+        
+//        ローカル通知が発動するtrigger(日付マッチ)を作成
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: task.date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+//      identifier,content, triggerからローカル通知を作成
+        let request = UNNotificationRequest(identifier: String(task.id), content: content, trigger: trigger)
+        
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in
+            print(error ?? "ローカル通知登録　OK")
+//   error が nil ならローカル通知の登録に成功したと表示。errorが存在すればerrorを表示。
+        }
+        
+        center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+            for request in requests {
+            print("/---------------")
+            print(request)
+            print("---------------/")
+        
+            }
+        }
+    }
+    
+    @objc func dismissKeyboard(){
+//         キーボードを閉じる
+           view.endEditing(true)
 
     /*
     // MARK: - Navigation
@@ -31,4 +117,5 @@ class inputViewController: UIViewController {
     }
     */
 
+}
 }
